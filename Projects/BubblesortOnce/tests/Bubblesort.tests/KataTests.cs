16bf424cf8d75391f7454f54a13d6c21f05fcdf2
mendel <mendel.agterberg@gmail.com>
2017-07-22 16:32:47 +0200

    Added first step for bubblesort

--- Projects/BubblesortOnce/tests/Bubblesort.tests/KataTests.cs
+++ Projects/BubblesortOnce/tests/Bubblesort.tests/KataTests.cs
@@ -10,11 +10,33 @@ namespace Bubblesort.tests
         public void Dependencies_Should_Be_Hooked_Up()
         {
             // Arrange
-                var kata = new Kata();
+            var kata = new Kata();
             // Act
-                var result = kata.BubbleSortOnce(new int[] { 3, 4, 5 });
+            var actual = kata.BubbleSortOnce(new int[] { 3, 4, 5 });
             // Assert
-                Assert.Equal(3, result.Length);
+            Assert.Equal(3, actual.Length);
         }
+
+        [Fact]
+        public void One_Step_Should_Return_Modified_arr()
+        {
+            var kata = new Kata();
+            // Example test case from description
+            var input = new int[] { 7, 5};
+       
+            var expected = new int[] { 5, 7 };
+
+            Assert.Equal(expected, kata.BubbleSortOnce(input));
+        }
+
+        // [Fact]
+        // public void ExampleTest()
+        // {
+        //     var kata = new Kata();
+        //     // Example test case from description
+        //     var expected = new int[] { 7, 5, 3, 1, 2, 4, 6, 8, 9 };
+        //     var actual = new int[] { 9, 7, 5, 3, 1, 2, 4, 6, 8 };
+        //     Assert.Equal(expected, kata.BubbleSortOnce(actual));
+        // }
     }
 }
