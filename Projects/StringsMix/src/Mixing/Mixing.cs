77c779c8335014fe7ce6284a6074066af6ad68ce
mendel <mendel.agterberg@gmail.com>
2017-07-23 09:27:46 +0200

    Added better sorting and cleaned up tests

--- Projects/StringsMix/src/Mixing/Mixing.cs
+++ Projects/StringsMix/src/Mixing/Mixing.cs
@@ -1,16 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Mixing
 {
     public class Mixer
     {
-        public string Mix(string s1, string s2)
-        {
-
-            return "asd";
-        }
-
         public int CharCounter(string input, char character)
         {
             var result = input.Where(c => c == character);
@@ -18,21 +13,24 @@ namespace Mixing
             return result.Count();
         }
 
-        public string FormatInput(string input)
+        public List<string> FormatResult(List<string> input)
         {
-            return new string(input.OrderBy(c => c).ToArray());
+            var result = input.OrderByDescending(r => r.Length).ThenBy(r => r[0] == '=').ThenBy(r => r).ToList();
+
+            return result;
         }
 
-        public string FormatCombine(string s1, string s2)
+        public string Mix(string s1, string s2)
         {
-            var x = (s1 + s2).Distinct();
+            var UniqueCharacters = (s1 + s2).Distinct();
 
-            var result = "";
+            var combinedResult = new List<string>();
 
-            foreach (var character in x)
+            foreach (var character in UniqueCharacters)
             {
-                if (char.IsLetter(character))
+                if (char.IsLetter(character) && character == char.ToLower(character) )
                 {
+                    var chunk = string.Empty;
 
                     var results1 = CharCounter(s1, character);
                     var resultts2 = CharCounter(s2, character);
@@ -43,27 +41,47 @@ namespace Mixing
                     {
                         if (results1 > resultts2)
                         {
-                            result += "1:";
+                            chunk += "1:";
 
                         }
                         else if (results1 < resultts2)
                         {
-                            result += "2:";
+                            chunk += "2:";
                         }
                         else
                         {
-                            result += "=:";
+                            chunk += "=:";
                         }
 
                         for (int i = 0; i < total; i++)
                         {
-                            result += character;
+                            chunk += character;
                         }
-                        result += "/";
+
+                        chunk += "/";
+
+                        combinedResult.Add(chunk);
+
                     }
                 }
             }
-            return result.Substring(0, result.Length - 1);
+
+            combinedResult = FormatResult(combinedResult);
+            var result = "";
+
+            foreach (var chunk in combinedResult)
+            {
+                result += chunk;
+            }
+
+            if (result.Length >= 1)
+            {
+                return result.Substring(0, result.Length - 1);
+            }
+            else
+            {
+                return "";
+            }
         }
     }
 }
--- Projects/StringsMix/tests/Mixing.Tests/UnitTest1.cs
+++ Projects/StringsMix/tests/Mixing.Tests/UnitTest1.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using Mixing;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Mixing.Tests
 {
@@ -24,22 +25,49 @@ namespace Mixing.Tests
             Assert.Equal(expected, actual);
         }
 
-        [Theory]
-        [InlineData("ccccaaaddd", "aaaccccddd")]
-        public void Should_Format_String(string input, string expected)
+        [Fact]
+        public void Shoud_Sort_Stuff()
         {
-            var actual = _mixer.FormatInput(input);
+            var actual = new List<string>() { "2:bbbb", "1:aaaaaaa", "=:bb" };
+            var exptected = new List<string>() { "1:aaaaaaa", "2:bbbb", "=:bb" };
+            var result = _mixer.FormatResult(actual);
 
-            Assert.Equal(expected, actual);
+            Assert.Equal(exptected, result);
+        }
+
+        [Fact]
+        public void Shoud_Sort_Symbols()
+        {
+            var actual = new List<string>() { "2:bbbb", "1:bbbb", "=:bbbb" };
+            var exptected = new List<string>() { "1:bbbb", "2:bbbb", "=:bbbb" };
+            var result = _mixer.FormatResult(actual);
+
+            Assert.Equal(exptected, result);
+        }
+
+        [Fact]
+        public void Shoud_Sort_SymbolsandChar()
+        {
+            var actual = new List<string>() { "2:cccc", "1:bbbb", "1:dddd", "=:qq" };
+            var exptected = new List<string>() { "1:bbbb", "1:dddd", "2:cccc", "=:qq" };
+            var result = _mixer.FormatResult(actual);
+
+            Assert.Equal(exptected, result);
         }
 
         [Theory]
         [InlineData("aabb", "aaabbc", "2:aaa/=:bb")]
         [InlineData("aabb!@#!@#", "aaabbc@#$@#$333", "2:aaa/=:bb")]
         [InlineData("@#$//aabb!@#!@#////", "aaabbc@#$@#$333", "2:aaa/=:bb")]
+        [InlineData("Are they here", "yes, they are here", "2:eeeee/2:yy/=:hh/=:rr")]
+        [InlineData("looping is fun but dangerous", "less dangerous than coding", "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg")]
+        [InlineData(" In many languages", " there's a pair of functions", "1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt")]
+        [InlineData("Lords of the Fallen", "gamekult", "1:ee/1:ll/1:oo")]
+        [InlineData("codewars", "codewars", "")]
+        [InlineData("A generation must confront the looming ", "codewarrs", "1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr")]
         public void Should_Format_String2(string s1, string s2, string expected)
         {
-            var actual = _mixer.FormatCombine(s1, s2);
+            var actual = _mixer.Mix(s1, s2);
 
             Assert.Equal(expected, actual);
         }
