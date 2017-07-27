bdb152808f208a52688895ac649e2a2b1db61e62
mendel <mendel.agterberg@gmail.com>
2017-07-27 19:15:55 +0200

    Added all tests for first rotation

--- Projects/DoubleCola/tests/DoubleCola.Tests/LineTests.cs
+++ Projects/DoubleCola/tests/DoubleCola.Tests/LineTests.cs
@@ -7,28 +7,31 @@ namespace DoubleCola.Tests
     public class LineTests
     {
         private readonly Line _line;
+        private readonly string[] names;
         public LineTests()
         {
             this._line = new Line();
+            names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
         }
         [Fact]
         public void WhoIsNext_Should_Return_Name_WithOut_Doubleing()
         {
-            string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
-        
-            var expected = _line.WhoIsNext(names, 0);
-            
-            Assert.Equal(names[0], expected);
+
+            var actually = _line.WhoIsNext(names, 0);
+
+            Assert.Equal(names[0], actually);
         }
 
-        [Fact]
-        public void WhoIsNext_Should_Return_Name_After_First_Rotation()
+        [Theory]
+        [InlineData(1, "Leonard")]
+        [InlineData(2, "Penny")]
+        [InlineData(3, "Rajesh")]
+        [InlineData(4, "Howard")]
+        public void WhoIsNext_Should_Return_Name_After_First_Rotation(long n, string expected)
         {
-            string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
-        
-            var expected = _line.WhoIsNext(names, 1);
-            
-            Assert.Equal("Leonard", expected);
+            var actually = _line.WhoIsNext(names, n);
+
+            Assert.Equal(expected, actually);
         }
     }
 }
