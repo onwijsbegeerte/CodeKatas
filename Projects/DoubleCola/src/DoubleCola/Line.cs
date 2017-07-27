fe4cee0f5f529c1e646bf2b548e47b841e9f464d
mendel <mendel.agterberg@gmail.com>
2017-07-27 19:10:10 +0200

    Added first loging for queue

--- Projects/DoubleCola/src/DoubleCola/Line.cs
+++ Projects/DoubleCola/src/DoubleCola/Line.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoubleCola
 {
     public class Line
     {
         public string WhoIsNext(string[] names , long n)
-        { 
-              return names[0];
+        {
+            Queue<string> queue = new Queue<string>(names);
+           
+            for (int i = 0; i < n; i++)
+            {
+                var first = queue.Dequeue();
+
+
+            }
+
+            return queue.Dequeue();
         }
     }
 }
--- Projects/DoubleCola/tests/DoubleCola.Tests/LineTests.cs
+++ Projects/DoubleCola/tests/DoubleCola.Tests/LineTests.cs
@@ -6,15 +6,29 @@ namespace DoubleCola.Tests
 {
     public class LineTests
     {
+        private readonly Line _line;
+        public LineTests()
+        {
+            this._line = new Line();
+        }
         [Fact]
         public void WhoIsNext_Should_Return_Name_WithOut_Doubleing()
         {
             string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
-            var line = new Line();
         
-            var expected = line.WhoIsNext(names, 0);
+            var expected = _line.WhoIsNext(names, 0);
             
             Assert.Equal(names[0], expected);
+        }
+
+        [Fact]
+        public void WhoIsNext_Should_Return_Name_After_First_Rotation()
+        {
+            string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+        
+            var expected = _line.WhoIsNext(names, 1);
+            
+            Assert.Equal("Leonard", expected);
         }
     }
 }
